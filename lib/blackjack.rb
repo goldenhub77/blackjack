ce1c3f243678f86ffb2e8d0a9a8dddecf1c15425
goldenhub77 <sg300c@gmail.com>
2017-02-24 16:32:49 -0500

    More refactoring of blackjack.rb

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,10 +1,11 @@
 require_relative "card"
 require_relative "deck"
 require_relative "hand"
+require 'pry'
 
 class BlackJack
+  attr_reader :players_hand, :dealers_hand, :terminal_output
   attr_accessor :hit_stand
-  attr_reader :players_hand, :dealers_hand
 
   WELCOME = "
   Welcome to Black Jack!\n
@@ -12,54 +13,42 @@ class BlackJack
   1. Dealer wins ties unless ties are blackjacks\n
   2. Dealer will hit until score of at least 17 is reached\n\n"
 
-  def initialize(hit_stand = nil)
-    @hit_stand = hit_stand
+  def initialize
+    @hit_stand = nil
     @game_deck = Deck.new
     @players_hand = Hand.new(@game_deck.draw(2))
     @dealers_hand = Hand.new(@game_deck.draw(2))
     @terminal_output = ""
-
     main
-  end
-
-  def add_terminal_output(input)
-    if @terminal_output.include?(input)
-      @terminal_output = ""
-    else
-      @terminal_output += input
-    end
-    nil
   end
 
   def main
     print WELCOME
     while @players_hand.calculate_hand <= 21 && !['s', 'stand'].include?(@hit_stand)
-
       if ['s', 'stand'].include?(@hit_stand)
         end_game
       elsif ['h', 'hit'].include?(@hit_stand)
         hit
       end
 
-      add_terminal_output("Player was dealt: ")
-      @players_hand.cards.map {|card| add_terminal_output(card.rank+card.suit + " ")}
-      add_terminal_output("/ Player's score: #{@players_hand.calculate_hand}\n")
-      @hit_stand = nil
-
-      while !['s','h','stand','hit'].include?(@hit_stand) && @players_hand.calculate_hand < 21
-        print @terminal_output
-        print "\nDo you want to hit or stand? > "
-        @hit_stand = gets.chomp.downcase
+      player
+      #&& @players_hand.calculate_hand <= 21
+      while @players_hand.calculate_hand <= 21 && !['s','h','stand','hit'].include?(@hit_stand)
+        user_input
       end
     end
    end_game
   end
 
   def game_results
-    if @players_hand.calculate_hand > @dealers_hand.calculate_hand
-      @players_hand.calculate_hand > 21 ? (add_terminal_output("\nplayer bust")) : (add_terminal_output("\nplayer won"))
+    if @players_hand.calculate_hand > 21
+      add_terminal_output("\nplayer bust, dealer won")
+    elsif @players_hand.calculate_hand > @dealers_hand.calculate_hand
+      add_terminal_output("\nplayer won")
+    elsif @dealers_hand.calculate_hand > 21
+      add_terminal_output("\ndealer bust, player won")
     elsif @players_hand.calculate_hand < @dealers_hand.calculate_hand
-      @dealers_hand.calculate_hand > 21 ? (add_terminal_output("\ndealer bust, player won")) : (add_terminal_output("\ndealer won"))
+      add_terminal_output("\ndealer won")
     elsif @players_hand.calculate_hand == 21 && @dealers_hand.calculate_hand == 21
       add_terminal_output('push')
       'push'
@@ -69,8 +58,24 @@ class BlackJack
     end
   end
 
+  def user_input
+    print @terminal_output
+    print "\nDo you want to hit or stand? > "
+    @hit_stand = gets.chomp.downcase
+  end
+
   def hit
+    @hit_stand = nil
     @players_hand.cards += @game_deck.draw
+  end
+
+  def player
+    @terminal_output = ""
+    add_terminal_output("Player was dealt: ")
+    @players_hand.cards.map do |card|
+      add_terminal_output(card.rank+card.suit + " ")
+    end
+    add_terminal_output("/ Player's score: #{@players_hand.calculate_hand}\n")
   end
 
   def dealer
@@ -85,11 +90,14 @@ class BlackJack
     @dealers_hand
   end
 
+  def add_terminal_output(input)
+    @terminal_output += input
+    nil
+  end
+
   def end_game
     dealer
     game_results
-    puts @terminal_output
+    print @terminal_output
   end
 end
-game_one = BlackJack.new
-game_one
